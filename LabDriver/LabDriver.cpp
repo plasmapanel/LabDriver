c4fd984e86438336f1b19f341c3f3765d151987e
mausilio <mausilio@umich.edu>
2015-10-16 14:48:47 -0400

    renamed main

--- LabDriver/LabDriver.cpp
+++ LabDriver/LabDriver.cpp
@@ -1,63 +1,12 @@
 #include "WeinerCounter.h"
-#include "headers.h"
 #include "stdafx.h"
-#include <ctime>
-#include <chrono>
-#include <thread>
-#include <vector>
-#include <cmath>
-#include <fstream>
-#include <queue>
 #include <iostream>
-#include "HighResClock.h"
 #include "LabUtilities.h"
-#include "VoltageControl.h"
 #include "MotorController.h"
-/*
-#include "TCanvas.h"
-#include "TH1.h"
-#include "TError.h"
-#include "TSystem.h"
-*/
 #include "messages.h"
 using namespace std;
 
 int main(){
-  //doAfterScanGraphMulti();
-  //doVoltageScan();
-  //makeTChainN(".\\CollectedData\\Ne1Ar20CF4\\740torr\\MC02_Ne1Ar20CF4_740_1436473464\\AfterPulse\\");
-  //makeApGraphsN(".\\CollectedData\\Ne1Ar20CF4\\740torr\\MC02_Ne1Ar20CF4_740_1436814619\\AfterPulse\\", 100);
-  //makeApDiffGraphN(".\\CollectedData\\Ne1Ar20CF4\\740torr\\MC02_Ne1Ar20CF4_740_1436814619\\AfterPulse\\");
-  //makeApIntGraphN(".\\CollectedData\\Ne1Ar20CF4\\740torr\\MC02_Ne1Ar20CF4_740_1436814619\\AfterPulse\\", 100);
-  //WeinerCounter *nim = new WeinerCounter(0);
-  //vector<string> lines = { "Asd" };
-  //findLineWithLowestRate(nim, lines);
-  //findHighestRate(nim, lines);
-  //HeaderInfoGen hg;
-  //doVoltageScan();
-  //doWeinerCount(nim, 10 * 60, 1, 1080, hg, lines, "asd.txt");
-  //doAfterPulse10("test.txt", nim, hg, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,1000,10000);
-  
-  //MotorController mot(6, 9600, "mc.pix");
-  //cout << mot.getAbsolutePositionX() << endl;
-  /*
-  mot.stepMotor(2, -20000);
-  mot.stepMotor(1, 20000);
-  
-  mot.setZero();
-  mot.stepMotor(2, 20000);
-  mot.stepMotor(1, -20000);
-  */
-  /*
-  for (int i = 0; i < mot.getNumPixActive(); ++i){
-    mot.moveToPix(i);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-  }
-  */
-  //mot.goZero();
-  
-  //delete nim;
-
   //initialize lab hardware
   char ch;
   WeinerCounter *nim = nullptr;
